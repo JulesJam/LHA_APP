b34f5276c1c6e9a94a898eab96f36594d859be5c
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2019-04-04 10:32:37 +0100

    Added main image using temporary drop box link and added about page but need proper copy to be added

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -11,9 +11,8 @@ export class BlogComponent {
   private iframeURL: any
  
   constructor(private sanitizer: DomSanitizer) {
-    console.log("ifu", this.iframeURL);
+  
    this.iframeURL  = sanitizer.bypassSecurityTrustResourceUrl( "https://embed.tumblr.com/embed/post/zV81tYHrlsC2KIYsM898WA/176872968312");
-    console.log("Here is the iframe", this.iframeURL);
    }
 
   ngOnInit() {
