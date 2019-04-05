982859ad758d4ae09ae7d46e99418225fab34f61
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2019-04-05 18:41:17 +0100

    added menu toggle to works menu for ios need to auto remove when active

--- src/app/top-navigation/top-navigation.component.ts
+++ src/app/top-navigation/top-navigation.component.ts
@@ -7,12 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavigationComponent implements OnInit {
 
+  worksMenuOpen: boolean;
+  message: string
 
 
   constructor() { }
 
   ngOnInit() {
-   
+   this.worksMenuOpen = false;
+   this.message = "apple";
+  }
+
+  toggleMenu(): void {
+
+    this.worksMenuOpen =! this.worksMenuOpen;
+    this.worksMenuOpen? this.message = "fart" : this.message = "apple";
   }
 
 
