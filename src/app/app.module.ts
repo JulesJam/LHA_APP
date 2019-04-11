371ce90cee4e2d53ba95ced2d546335364a08917
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2019-04-11 18:59:25 +0100

    testing angular animations as way toadd remove image slider on project selection - need to check out violation on image scrolling and choose better animation but in principle this works. Also need to have an emitter for close function to remove image slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { NgImageSliderModule } from 'ng-image-slider';
@@ -40,6 +41,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     NgImageSliderModule
   ],
--- src/app/project-gallery/project-gallery.component.ts
+++ src/app/project-gallery/project-gallery.component.ts
@@ -1,22 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations'
 import { ImageSliderComponent } from '../image-slider/image-slider.component';
 
 
 @Component({
   selector: 'app-project-gallery',
   templateUrl: './project-gallery.component.html',
-  styleUrls: ['./project-gallery.component.css']
+  styleUrls: ['./project-gallery.component.css'],
+  animations: [
+    trigger('changeDivSize', [
+      state('initial', style({
+        display: 'none'
+      })),
+      state('final', style({
+       position: 'absolute',
+       backgroundColor: 'rgba(173, 173, 173, .8)',
+       width: '100%',
+       height: '88vh',
+       maxHeight: '88vh',
+       top: 0,
+       left: 0,
+       zIndex: 100
+      })),
+      transition('initial=>final', animate('1500ms')),
+      transition('final=>initial', animate('1000ms'))
+    ]),
+  ]
+
 })
 export class ProjectGalleryComponent implements OnInit {
 
   private projects: Array<{id: number, imageUrl: string, title: string, year: number, blurb: string}>;
 
   private blurb: string;
+  private displayProjectState: string;
+
 
 
   constructor() { }
 
   ngOnInit() {
+
+    this.displayProjectState = 'initial';
 
     this.blurb = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Ornare suspendisse sed nisi lacus sed viverra tellus in hac. Convallis aenean et tortor at risus viverra. Etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum. Pharetra et ultrices neque ornare. Auctor elit sed vulputate mi. Amet est placerat in egestas erat imperdiet sed. Eget duis at tellus at. Scelerisque felis imperdiet proin fermentum leo vel. Sodales neque sodales ut etiam sit amet nisl purus in. Bibendum est ultricies integer quis auctor. Elementum sagittis vitae et leo duis'
 
@@ -51,6 +76,12 @@ export class ProjectGalleryComponent implements OnInit {
       year: 2017,
       blurb: this.blurb }
     ]
+  }
+
+  displayToggle():  void {
+    console.log("Switching state -pgc l 83", this.displayProjectState);
+    this.displayProjectState = this.displayProjectState === 'initial' ? 'final' : 'initial';
+ 
   }
 
   
